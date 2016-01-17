eee76700a28598bf6806e1e2a4d868790a00b451
jtfengwolf <jt.feng.wolf@outlook.vom>
2016-01-17 16:40:15 +0800

    maxpooling

---
+++ include/layer/MaxPoolingLayer.h
@@ -0,0 +1,36 @@
+#ifndef __FUDANDNN_MAXPOOLING_LAYER_H
+#define __FUDANDNN_MAXPOOLING_LAYER_H
+
+#include "layer/Layer.h"
+#include "util/Matrix.h"
+
+class MaxPoolingLayer : public Layer
+{
+public:
+	MaxPoolingLayer(size_t rowSize, size_t columnSize, size_t krowSize, size_t kcolumnSize, size_t visualSize,
+		size_t hiddenSize, size_t stride, size_t initScheme, double dropoutRate, shared_ptr<Solver> solver);
+	~MaxPoolingLayer();
+	string getNetworkName() override;
+	void compute() override;
+	void calculate() override;
+	void gradient() override;
+	void update();
+
+	void writeSelf(string) override;
+	void readSelf(string) override;
+	size_t getSize();
+	void init() override;
+
+private:
+	void initGradient();
+	bool initialized = false;
+	size_t krowSize = 0;
+	size_t kcolumnSize = 0;
+	size_t stride;
+	double dropoutRate;
+	shared_ptr<MatrixPool> instance;
+
+
+};
+
+#endif
---
+++ src/layer/MaxPoolingLayer.cpp
@@ -0,0 +1,77 @@
+#include "layer/MaxPoolingLayer.h"
+
+MaxPoolingLayer::MaxPoolingLayer(size_t rowSize, size_t columnSize, size_t krowSize, size_t kcolumnSize,
+	size_t visualSize, size_t hiddenSize, size_t stride, size_t initScheme, double dropoutRate, shared_ptr<Solver> solver)
+{
+	this->visualRow = rowSize;
+	this->visualColumn = columnSize;
+	this->krowSize = krowSize;
+	this->kcolumnSize = columnSize;
+	this->hiddenRow = rowSize - krowSize + 1;;
+	this->hiddenColumn = columnSize - kcolumnSize + 1;
+	this->visualSize = visualSize;
+	this->hiddenSize = hiddenSize;
+	this->stride = stride;
+	this->solver = solver;
+	this->dropoutRate = dropoutRate;
+	this->instance = MatrixPool::getInstance();
+	this->init();
+}
+
+MaxPoolingLayer::~MaxPoolingLayer()
+{
+	for (size_t v = 0; v < visualSize; v++)
+	{
+		visualGradient.push_back(instance->allocMatrixUnclean(visualRow, visualColumn));
+	}
+
+	for (size_t h = 0; h < hiddenSize; h++)
+	{
+		hiddenGradient.push_back(instance->allocMatrixUnclean(hiddenRow, hiddenColumn));
+	}
+	initialized = true;
+}
+
+void MaxPoolingLayer::compute()
+{
+	for (size_t i = 0; i < hiddenSize; i++)
+	{
+		hiddenValue[i] = visualValue[i]->maxSubSampling(krowSize, kcolumnSize, stride);
+	}
+}
+
+void MaxPoolingLayer::calculate()
+{
+	for (size_t i = 0; i < hiddenSize; i++)
+	{
+		hiddenValue[i] = visualValue[i]->maxSubSampling(krowSize, kcolumnSize, stride);
+	}
+}
+
+void MaxPoolingLayer::gradient()
+{
+	for (size_t i = 0; i < visualSize; i++)
+	{
+		visualGradient[i] = visualValue[i]->maxUpSampling(krowSize, kcolumnSize, stride, hiddenGradient[i]);
+	}
+}
+
+void MaxPoolingLayer::writeSelf(string filename)
+{
+
+}
+
+void MaxPoolingLayer::readSelf(string filename)
+{
+
+}
+
+string MaxPoolingLayer::getNetworkName()
+{
+	return "MaxPoolingLayer";
+}
+
+void MaxPoolingLayer::update()
+{
+}
+
