9dc88dbe4de4bd29d31f5b1d2f90e53fc0dacea5
lisiyan <LI.SIYAN@hotmail.com>
2016-03-07 09:51:01 +0800

    fix bugs

--- src/util/XMLInput.cpp
+++ src/util/XMLInput.cpp
@@ -2,13 +2,16 @@
 
 XMLInput::XMLInput(string file, int id) :Input(file,id){
 	parser = shared_ptr<XMLDataParser>(new XMLDataParser(file));
+	cout << "read file to memory..." << endl;
 	this->samples = parser->getAllSamples();
+	cout << "read file done" << endl;
 	index = -1;
 }
 
 shared_ptr<Sample> XMLInput::getNextSample(){
 	index++;
-	index = index % 120;
+	index = index % this->samples.size();
+	//cout << index << " " << getSampleNum() << endl;
 	return samples.at(index);
 }
 
